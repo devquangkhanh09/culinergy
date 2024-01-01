3a5f37876c3c1a09cdaa45206381fe57af281414
hung-nhb <hung.nhb@gmail.com>
2024-01-01 21:35:03 +0700

    add delete button in allergenic ingredients

--- src/Screens/Allergenic/Allergenic.tsx
+++ src/Screens/Allergenic/Allergenic.tsx
@@ -2,23 +2,11 @@ import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
 import React, { useState } from 'react';
 import SelectionModal from '@/Components/Modal/SelectionModal';
 import { toggleIngredient } from '@/Store/reducers';
-
-const sampleIngredients = [
-  'Chicken',
-  'Beef',
-  'Pork',
-  'Fish',
-  'Shrimp',
-  'Egg',
-  'Milk',
-  'Cheese',
-  'Potato',
-  'Carrot',
-  'Tomato',
-]
+import { useLazyGetIngredientsQuery } from '@/Services/ingredients';
 
 export default function Allergenic() {
   const [isIngredientModalVisible, setIsIngredientModalVisible] = useState(false);
+  const [fetchIngredients, { data: ingredientsData }] = useLazyGetIngredientsQuery();
 
   const handleSelectionComplete = () => {
     setIsIngredientModalVisible(false);
@@ -27,11 +15,19 @@ export default function Allergenic() {
   return (
     <View style={styles.container}>
       <View style={{ borderRadius: 15, borderColor: 'rgba(173, 173, 173, 0.50)', borderWidth: 1 }}>
-        <View style={{ height: 50, backgroundColor: '#ffffff', paddingLeft: 20, justifyContent: 'center', borderTopLeftRadius: 15, borderTopRightRadius: 15 }}>
+        <View style={{ height: 50, backgroundColor: '#ffffff', paddingHorizontal: 20, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', borderTopLeftRadius: 15, borderTopRightRadius: 15 }}>
           <Text>Crab</Text>
+          <Image
+            source={require('../../../assets/settings/delete-allergenic.png')}
+            style={{ height: 20, width: 20 }}
+          />
         </View>
-        <View style={{ height: 50, backgroundColor: '#F1F1F1', paddingLeft: 20, justifyContent: 'center' }}>
+        <View style={{ height: 50, backgroundColor: '#F1F1F1', paddingHorizontal: 20, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
           <Text style={{ fontSize: 14 }}>Milk</Text>
+          <Image
+            source={require('../../../assets/settings/delete-allergenic.png')}
+            style={{ height: 20, width: 20 }}
+          />
         </View>
         <Pressable onPress={() => setIsIngredientModalVisible(true)} style={{ height: 50, backgroundColor: '#ffffff', flexDirection: 'row', alignItems: 'center', borderBottomLeftRadius: 15, borderBottomRightRadius: 15 }}>
           <Image
@@ -43,7 +39,7 @@ export default function Allergenic() {
         <SelectionModal
           isVisible={isIngredientModalVisible}
           title='Select ingredients'
-          options={sampleIngredients}
+          options={ingredientsData ? ingredientsData : []}
           storeKey='explore'
           reducer={toggleIngredient}
           onSelectionComplete={handleSelectionComplete}
